9e126f9bed8c1f9ebdf872383fc286f21052a338
VickusWan <vuong@mail.sfsu.edu>
2024-01-11 22:22:49 -0800

    connect four almost fully functional

--- static/connectfour.py
+++ static/connectfour.py
@@ -8,7 +8,6 @@ class connect4_board():
         self.num_rows = 6
         self.num_cols = 7
         self.board = np.zeros((self.num_rows, self.num_cols), dtype=int)
-        self.board[5, 3] = -1
         
     def get_state(self):
         return self.board
@@ -35,85 +34,199 @@ class connect4_board():
         new_instance.board = np.copy(self.board)
         return new_instance
     
-    def reset(self):
-        self.board = np.zeros((self.num_rows, self.num_cols), dtype=int)     
+    # def check_winner(self):
+    #     plus_score = heuristic(self, +1)
+    #     minus_score = heuristic(self, -1)
+    #     if plus_score == 10000:
+    #         document.getElementById('winner').innerText = 'AI wins!'
+    #         return True
+    #     elif minus_score == -10000:
+    #         document.getElementById('winner').innerText = 'Player wins!'
+    #         return True
+    #     else:
+    #         return False
+        
+        
+    def check_winner(self, game, player):
+        num_rows = 6
+        num_cols = 7
+        board = game.get_state()
+        for i in range(num_rows):
+            for num in range(4):
+                if (np.all(board[i,num:num+4]==player)):
+                    return True
+                
+        for i in range(num_cols):
+            for num in range(3):
+                if (np.all(board[num:num+4,i]==player)):
+                    return True
+                
+        for i in range(num_rows-3):
+            for j in range(3, num_cols):
+                diagonal = np.array([board[i, j], board[i+1, j-1], board[i+2, j-2], board[i+3, j-3]])
+                if (np.all(diagonal==player)):
+                    return True
+                
+        for i in range(num_rows-3):
+            for j in range(3, -1, -1):
+                diagonal = np.array([board[i, j], board[i+1, j+1], board[i+2, j+2], board[i+3, j+3]])
+                if (np.all(diagonal==player)):
+                    return True
+        return False    
+        
+    
+        
+    def me_first(self, event):
+        return
+    
+    def ai_first(self, event):
+        (col, score) = alphabeta(self, 4, float('-inf'), float('inf'), True)
+        row = self.play_move(1, col)
+        ai_location = 'cell'+str(row)+str(col)
+        document.getElementById(ai_location).style.backgroundColor = 'yellow'
+    
+    def newgame(self, event):
+        self.board = np.zeros((self.num_rows, self.num_cols), dtype=int)
+        for i in range(6):
+            for j in range(7):
+                location = 'cell'+str(i)+str(j)
+                document.getElementById(location).style.backgroundColor = 'white'
+        document.getElementById('winner').innerText = ''
+        return      
         
     def insert0(self, event):
+        if self.check_winner(self, 1):
+            document.getElementById('winner').innerText = 'AI wins!'
+            return
+        elif self.check_winner(self, -1):
+            document.getElementById('winner').innerText = 'Player wins!'
+            return
         row = self.play_move(-1, 0)
         location = 'cell'+str(row)+'0'
         document.getElementById(location).style.backgroundColor = 'red'
         
-        (col, _) = alphabeta(self, 4, float('-inf'), float('inf'), True)
+        (col, score) = alphabeta(self, 4, float('-inf'), float('inf'), True)
         row = self.play_move(1, col)
         ai_location = 'cell'+str(row)+str(col)
         document.getElementById(ai_location).style.backgroundColor = 'yellow'
-        print(location, col, 'AI=', ai_location)
+        print(col, score)
+        if self.check_winner(self, 1):
+            document.getElementById('winner').innerText = 'AI wins!'
+            return
         
     def insert1(self, event):
+        if self.check_winner(self, 1):
+            document.getElementById('winner').innerText = 'AI wins!'
+            return
+        elif self.check_winner(self, -1):
+            document.getElementById('winner').innerText = 'Player wins!'
+            return
         row = self.play_move(-1, 1)
         location = 'cell'+str(row)+'1'
         document.getElementById(location).style.backgroundColor = 'red'
         
-        (col, _) = alphabeta(self, 4, float('-inf'), float('inf'), True)
+        (col, score) = alphabeta(self, 4, float('-inf'), float('inf'), True)
         row = self.play_move(1, col)
         ai_location = 'cell'+str(row)+str(col)
         document.getElementById(ai_location).style.backgroundColor = 'yellow'
-        print(location, col, 'AI=', ai_location)
+        if self.check_winner(self, 1):
+            document.getElementById('winner').innerText = 'AI wins!'
+            return
         
     def insert2(self, event):
+        if self.check_winner(self, 1):
+            document.getElementById('winner').innerText = 'AI wins!'
+            return
+        elif self.check_winner(self, -1):
+            document.getElementById('winner').innerText = 'Player wins!'
+            return
         row = self.play_move(-1, 2)
         location = 'cell'+str(row)+'2'
         document.getElementById(location).style.backgroundColor = 'red'
         
-        (col, _) = alphabeta(self, 4, float('-inf'), float('inf'), True)
+        (col, score) = alphabeta(self, 4, float('-inf'), float('inf'), True)
         row = self.play_move(1, col)
         ai_location = 'cell'+str(row)+str(col)
         document.getElementById(ai_location).style.backgroundColor = 'yellow'
-        print(location, col, 'AI=', ai_location)
+        if self.check_winner(self, 1):
+            document.getElementById('winner').innerText = 'AI wins!'
+            return
         
     def insert3(self, event):
+        if self.check_winner(self, 1):
+            document.getElementById('winner').innerText = 'AI wins!'
+            return
+        elif self.check_winner(self, -1):
+            document.getElementById('winner').innerText = 'Player wins!'
+            return
         row = self.play_move(-1, 3)
         location = 'cell'+str(row)+'3'
         document.getElementById(location).style.backgroundColor = 'red'
         
-        (col, _) = alphabeta(self, 4, float('-inf'), float('inf'), True)
+        (col, score) = alphabeta(self, 4, float('-inf'), float('inf'), True)
         row = self.play_move(1, col)
         ai_location = 'cell'+str(row)+str(col)
         document.getElementById(ai_location).style.backgroundColor = 'yellow'
-        print(location, col, 'AI=', ai_location)
+        if self.check_winner(self, 1):
+            document.getElementById('winner').innerText = 'AI wins!'
+            return
         
     def insert4(self, event):
+        if self.check_winner(self, 1):
+            document.getElementById('winner').innerText = 'AI wins!'
+            return
+        elif self.check_winner(self, -1):
+            document.getElementById('winner').innerText = 'Player wins!'
+            return
         row = self.play_move(-1, 4)
         location = 'cell'+str(row)+'4'
         document.getElementById(location).style.backgroundColor = 'red'
         
-        (col, _) = alphabeta(self, 4, float('-inf'), float('inf'), True)
+        (col, score) = alphabeta(self, 4, float('-inf'), float('inf'), True)
         row = self.play_move(1, col)
         ai_location = 'cell'+str(row)+str(col)
         document.getElementById(ai_location).style.backgroundColor = 'yellow'
-        print(location, col, 'AI=', ai_location)
+        if self.check_winner(self, 1):
+            document.getElementById('winner').innerText = 'AI wins!'
+            return
         
     def insert5(self, event):
+        if self.check_winner(self, 1):
+            document.getElementById('winner').innerText = 'AI wins!'
+            return
+        elif self.check_winner(self, -1):
+            document.getElementById('winner').innerText = 'Player wins!'
+            return
         row = self.play_move(-1, 5)
         location = 'cell'+str(row)+'5'
         document.getElementById(location).style.backgroundColor = 'red'
         
-        (col, _) = alphabeta(self, 4, float('-inf'), float('inf'), True)
+        (col, score) = alphabeta(self, 4, float('-inf'), float('inf'), True)
         row = self.play_move(1, col)
         ai_location = 'cell'+str(row)+str(col)
         document.getElementById(ai_location).style.backgroundColor = 'yellow'
-        print(location, col, 'AI=', ai_location)
+        if self.check_winner(self, 1):
+            document.getElementById('winner').innerText = 'AI wins!'
+            return
         
     def insert6(self, event):
+        if self.check_winner(self, 1):
+            document.getElementById('winner').innerText = 'AI wins!'
+            return
+        elif self.check_winner(self, -1):
+            document.getElementById('winner').innerText = 'Player wins!'
+            return
         row = self.play_move(-1, 6)
         location = 'cell'+str(row)+'6'
         document.getElementById(location).style.backgroundColor = 'red'
         
-        (col, _) = alphabeta(self, 4, float('-inf'), float('inf'), True)
+        (col, score) = alphabeta(self, 4, float('-inf'), float('inf'), True)
         row = self.play_move(1, col)
         ai_location = 'cell'+str(row)+str(col)
         document.getElementById(ai_location).style.backgroundColor = 'yellow'
-        print(location, col, 'AI=', ai_location)
+        if self.check_winner(self, 1):
+            document.getElementById('winner').innerText = 'AI wins!'
+            return
 
 def alphabeta(board, depth, alpha, beta, maximizingPlayer):
     if maximizingPlayer:
@@ -122,7 +235,7 @@ def alphabeta(board, depth, alpha, beta, maximizingPlayer):
         player = -1
         
     if depth == 0 or board.is_end():
-        score = heuristic(board, player) + heuristic(board, -player)
+        score = heuristic(board, player)
         return (None, score)
     
     if maximizingPlayer:
@@ -179,26 +292,32 @@ def heuristic(game, player):
             if (np.all(board[i,num:num+4]==0)):
                 continue
             elif (np.all(board[i,num:num+4]==player)):
-                  score = float('inf')
+                  score = 10000
             elif ((board[i,num:num+4] == player).sum() == 3) and ((board[i,num:num+4] == 0).sum() == 1):
                   score += 10       
             elif ((board[i,num:num+4] == player).sum() == 2) and ((board[i,num:num+4] == 0).sum() == 2):
                   score += 1              
             elif ((board[i,num:num+4] == -player).sum() == 1):
                   score += 0
+            # If opponent has 3 in a row
+            if ((board[i,num:num+4] == -player).sum() == 3):
+                  score -= 5
     # Vertical
     for i in range(num_cols):
         for num in range(3):
             if (np.all(board[num:num+4,i]==0)):
                 continue    
             elif (np.all(board[num:num+4,i]==player)):
-                  score = float('inf')
+                  score = 10000
             elif ((board[num:num+4,i] == player).sum() == 3) and ((board[num:num+4,i] == 0).sum() == 1):
                   score += 10        
             elif ((board[num:num+4,i] == player).sum() == 2) and ((board[num:num+4,i] == 0).sum() == 2):
                   score += 1              
             elif ((board[num:num+4,i] == -player).sum() == 1):
                   score += 0
+            # If opponent has 3 in a row
+            if ((board[num:num+4,i] == -player).sum() == 3):
+                score -= 5
     # Positive diagonal
     for i in range(num_rows-3):
         for j in range(3, num_cols):
@@ -206,7 +325,7 @@ def heuristic(game, player):
             if (np.all(diagonal==0)):
                 continue 
             elif (np.all(diagonal==player)):
-                  score = float('inf')
+                  score = 10000
             elif ((diagonal == player).sum() == 3) and ((diagonal == 0).sum() == 1):
                 if (board[i+1, j-1]==player) and (board[i+1, j-2]==player) and (board[i+1, j-3]==player):
                     score += 100
@@ -215,8 +334,11 @@ def heuristic(game, player):
             elif ((diagonal == player).sum() == 2) and ((diagonal == 0).sum() == 2):
                   score += 1             
             elif ((diagonal == -player).sum() == 1):
-                  score += 0        
-    
+                  score += 0
+            # If opponent has 3 in a row            
+            if ((diagonal == -player).sum() == 3):
+                score -= 5
+                
     # Negative diagonal
     for i in range(num_rows-3):
         for j in range(3, -1, -1):
@@ -224,7 +346,7 @@ def heuristic(game, player):
             if (np.all(diagonal==0)):
                 continue
             elif (np.all(diagonal==player)):
-                  score = float('inf')
+                  score = 10000
             elif ((diagonal == player).sum() == 3) and ((diagonal == 0).sum() == 1):
                 if (board[i+1, j+1]==player) and (board[i+1, j+2]==player) and (board[i+1, j+3]==player):
                     score += 100
@@ -235,6 +357,9 @@ def heuristic(game, player):
                   score += 1             
             elif ((diagonal == -player).sum() == 1):
                   score += 0
-    return player*score
+            # If opponent has 3 in a row
+            if ((diagonal == -player).sum() == 3):
+                score -= 5
+    return score
     
 GAME = connect4_board()
